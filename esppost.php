8ccb868185e441f8d781a53d2ea9c826d852910e
Gabriel Araujo <gabriel.fp.araujo@gmail.com>
2020-03-03 15:26:52 -0300

    Update esppost.php

--- esppost.php
+++ esppost.php
@@ -4,5 +4,7 @@
     $lon = $_POST["lon"];
     $Write = $lat . "," . $lon . "\n";
     $file_name = "node" . $node . ".txt";
+    print("Printing in file " . $node . ".txt")
+    print("the values lat:" . $lat . " and lon:" . $lon)
     file_put_contents($file_name, $Write);
 ?>
